c9941049284d7cbd046be2f1cb39762b036272fd
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-10 12:27:30 -0300

    feat: pinia with options api

--- src/stores/AuthUserStore.js
+++ src/stores/AuthUserStore.js
@@ -2,7 +2,12 @@ import { defineStore } from "pinia";
 export const useAuthUserStore = defineStore('AuthUserStore', {
     state: () => {
         return {
-            username: 'antoniontv'
+            username: 'antoniontv2'
+        }
+    },
+    actions: {
+        visitXProfile() {
+            window.open(`https://x.com/${this.username}`, '_blank')
         }
     }
 })
